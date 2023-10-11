41598c364ae7a20ec2215ce9aeb0cc978c409157
Octav85 <plopaoctavian@yahoo.com>
2023-10-11 13:57:23 +0300

    Added the implementation of a library system by using the collections

---
+++ src/main/java/com/octavian/bookcollectiontask/Main.java
@@ -0,0 +1,13 @@
+package com.octavian.bookcollectiontask;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] tablouDeDateInt = {4, 5, 7, 1, 2, 3, 254, };
+        System.out.println(IntNumberArrayService.getMinValueInedxOfArray(tablouDeDateInt));
+        System.out.println(IntNumberArrayService.getMinValueOfArray(tablouDeDateInt));
+        System.out.println(IntNumberArrayService.getAverageOfArray(tablouDeDateInt));
+
+    }
+
+
+}
